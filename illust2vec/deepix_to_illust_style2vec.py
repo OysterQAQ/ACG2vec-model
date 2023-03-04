a3afb72bec097475ac5af49b987799bba60fd54a
OysterQAQ <oysterqaq@Gmail.com>
2023-03-04 20:46:45 +0800

    init

--- illust2vec/deepix_to_illust_style2vec.py
+++ illust2vec/deepix_to_illust_style2vec.py
@@ -14,7 +14,7 @@ def gram_matrix(input_tensor):
   return result/(num_locations)
 
 
-model = keras.models.load_model('/Volumes/Data/oysterqaq/PycharmProjects/ACG2vec-model/deepix/model-resnet_custom_v3_float16.h5', compile=False)
+model = keras.models.load_model('/Volumes/Data/oysterqaq/PycharmProjects/ACG2vec-model/deepix/model-resnet_custom_v3.h5', compile=False)
 add = gram_matrix(model.get_layer('add').output)
 add_1 = gram_matrix(model.get_layer('add_1').output)
 add_2 = gram_matrix(model.get_layer('add_2').output)
@@ -26,5 +26,5 @@ output=GlobalMaxPooling1D()(stack)
 style_feature_extract_model = keras.Model(inputs=model.input, outputs=output)
 style_feature_extract_model.summary()
 
-style_feature_extract_model.save("/Volumes/Data/oysterqaq/Desktop/style_feature_extract_model_f16")
+#style_feature_extract_model.save("/Volumes/Data/oysterqaq/Desktop/style_feature_extract_model_f32")
 
